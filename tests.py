625b640192f2f3257f138bf1e33b42ffa9829753
Megan Peterson <Meganerd@Megans-MacBook-Pro.local>
2016-03-03 17:12:15 -0800

    formatting like a champion

--- tests.py
+++ tests.py
@@ -1,1 +1,35 @@
-tests.py
+"""Tests for Marvel Cinematic Universe app."""
+
+import unittest
+from server import app
+
+
+
+#test functions that render a template.
+
+def test_homepage(self):
+    """Tests to see if homepage renders."""
+
+    result = self.client.get('/')
+
+    self.assertEqual(result.status_code, 200)
+    self.assertIn('text/html', result.headers['Content-Type'])
+    self.assertIn('<a href="/movie_list" class="btn btn-lg btn-primary">ENTER</a>', result.data)
+
+def test_movie_list(self):
+    """Tests to see if movie_list page renders."""
+
+    result = self.client.get('/movie_list')
+
+    self.assertEqual(result.status_code, 200)
+    self.assertIn('text/html'), result.headers['Content-Type'])
+    self.assertIn('MOVIES',result.data)
+
+def test_characters(self):
+    """Test to see if the characters_list page renders."""
+
+    result = self.client.get('/characters')
+
+    self.assertEqual(results.status_code, 200)
+    self.assertIn('text/html'), result.headers['Content-Type'])
+    self.assertEqual('CHARACTERS', result.data)
